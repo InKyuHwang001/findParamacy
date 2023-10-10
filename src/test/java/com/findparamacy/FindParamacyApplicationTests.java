876913a09a3085c2a013c9bbe1be54d35601fefc
InGyu Hwang <hwanginkyu42@naver.com>
2023-10-10 23:01:48 +0900

    feat: init project

---
+++ src/test/java/com/findparamacy/FindParamacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findparamacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindParamacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
